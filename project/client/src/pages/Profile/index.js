9ad04df0ee47f661a5b9c9a94cf7924bf17745d0
Amanyadav207 <aman.23bcs10055@ms.sst.scaler.com>
2024-07-18 23:10:32 +0530

    profile page axios added

--- project/client/src/pages/Profile/index.js
+++ project/client/src/pages/Profile/index.js
@@ -2,9 +2,23 @@ import { Tabs } from 'antd';
 import Bookings from './Bookings';
 
 const Profile = () => {
-    // const onChange = (key) => {
-    //     console.log(key);
-    //   };
+  const navigate = useNavigate();
+  
+      const checkUser = async () => {
+        const user = await axios.get("/api/users/get-current-user", {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            }
+        });
+
+        if (user.data.data.role === "partner" || user.data.data.role === "admin") {
+            navigate("/partner");
+            message.error("You are not allowed to access this page");
+        } else {
+
+        }
+    }
+    checkUser();
       const items = [
         {
           key: '1',
